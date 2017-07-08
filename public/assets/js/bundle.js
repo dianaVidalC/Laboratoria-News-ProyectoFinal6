0e1b5e5238e6851da78096ba79c58caf156119f9
Erika Vidal <diann852@gmail.com>
2017-07-07 20:59:02 -0500

    noticia principal

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -3,46 +3,108 @@ const render = (root) => {
     root.empty();
     const wrapper = $('<div class="wrapper"></div>');
     wrapper.append(barraSuperior());
+    wrapper.append(barraInferior());
+    wrapper.append(noticiaPrincipal());
     root.append(wrapper);
 }
 
+const state={
+    newTitle:null
+}
+
 $( _ => {
-          const root = $('.root');
-          render(root);
+    $.get('http://localhost:3000/api/news')
+        .done(function (response) {
+            state.newTitle=response;
+            const root = $('.root');
+            render(root);
+        });
+
 });
 
 const barraSuperior=_=>{
     const navBar         = $('<nav class="navbar navbar-default"></nav>');
-    //const titulo         =$('<h1>hola</h1>');
     const container      = $('<div class="container"></div>');
-    const navHeader      = $('<div class="navbar-header"></div>');
-    const button         = $('<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"></button>');
-    const span           = $('<span class="sr-only">Toggle navigation</span>');
-    const iconBar1       = $('<span class="icon-bar"></span>');
-    const iconBar2       = $('<span class="icon-bar"></span>');
-    const iconBar3       = $('<span class="icon-bar"></span>');
-    const nameSections   = $('<a class="navbar-brand" href="#"><span class="glyphicon glyphicon-menu-hamburguer" aria-hidden="true"></span>SECTIONS</a>');
+    const navHeader      = $('<div class="navbar-header hidden-xs"></div>');
+    const nameSections   = $('<a class="navbar-brand" href="#">SECTIONS</a>');
+    const imgMenu        = $('<img alt="menu" src="assets/img/menu.png">');
+    const nameSearch     = $('<a class="navbar-brand" href="#">SEARCH</a>');
+    const imgSearch      = $('<img alt="menu" src="assets/img/search.png">');
+    const navRigth       = $('<div class="navbar-right social hidden-xs"></div>');
+    const image1         = $('<img class="social__img" alt="facebook-icono" src="assets/img/fb.png">');
+    const image2         = $('<img class="social__img" alt="twiter-icono" src="assets/img/tw.png">');
+    const image3         = $('<img class="social__img" alt="linkedin-icono" src="assets/img/in.png">');
 
-    const navBarCollapse = $('<div class="navbar-collapse collapse"></div>');
-    const ul             = $('<ul class="nav navbar-nav"></ul>');
-    const home           = $('<li class="active"><a href="#">Home</a></li>');
-    const about          = $('<li><a href="#about">About</a></li>');
-    const contact        = $('<li><a href="#contact">Contact</a></li>');
-
-    button.append(iconBar1);
-    button.append(iconBar2);
-    button.append(iconBar3);
-    navHeader.append(button);
+    navHeader.append(imgMenu);
     navHeader.append(nameSections);
+    navHeader.append(imgSearch);
+    navHeader.append(nameSearch);
+    navRigth.append(image1);
+    navRigth.append(image2);
+    navRigth.append(image3);
 
-    ul.append(home);
-    ul.append(about);
-    ul.append(contact);
-    navBarCollapse.append(ul);
     container.append(navHeader);
-    container.append(navBarCollapse);
+    container.append(navRigth);
     navBar.append(container);
 
     return navBar;
 
 }
+
+const barraInferior=()=>{
+    const div            = $('<div class="container-inferior"></div>');
+    const img            = $('<img class="img-responsive  align-center" src="assets/img/logoicon.png">');
+    const navBarCollapse = $('<div id="bs-example-navbar-collapse-1" class="navbar-collapse collapse nav-responsive"></div>');
+    const button         = $('<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"></button>');
+    const span           = $('<span class="sr-only">Toggle navigation</span>');
+    const iconBar1       = $('<span class="icon-bar"></span>');
+    const iconBar2       = $('<span class="icon-bar"></span>');
+    const iconBar3       = $('<span class="icon-bar"></span>');
+    const ul             = $('<ul class="nav navbar-nav"></ul>');
+    const ultimo         = $('<li><a class="col-white" href="#">Lo último</a></li>');
+    const opinion        = $('<li><a class="col-white" href="#">Opinión</a></li>');
+    const cultura        = $('<li><a class="col-white" href="#">Cultura</a></li>');
+    const peru           = $('<li><a class="col-white" href="#">Cultura</a></li>');
+    const tecnologia     = $('<li><a class="col-white" href="#">Tecnología</a></li>');
+    const mundo          = $('<li><a class="col-white" href="#">Mundo</a></li>');
+    const economia       = $('<li><a class="col-white" href="#">Economía</a></li>');
+    const lifestyle      = $('<li><a class="col-white" href="#">Lifestyle</a></li>');
+    const deporte        = $('<li><a class="col-white" href="#">Deporte</a></li>');
+
+    button.append(iconBar1);
+    button.append(iconBar2);
+    button.append(iconBar3);
+
+    ul.append(ultimo);
+    ul.append(opinion);
+    ul.append(cultura);
+    ul.append(peru);
+    ul.append(tecnologia);
+    ul.append(mundo);
+    ul.append(economia);
+    ul.append(lifestyle);
+    ul.append(deporte);
+    navBarCollapse.append(ul);
+    div.append(img);
+    navBarCollapse.append(button);
+    div.append(navBarCollapse);
+
+    return div;
+}
+
+const noticiaPrincipal=()=>{
+    const container= $('<div class="container container-news"></div>');
+    const img      = $('<img class="img-responsive" src="assets/img/news-0.png">');
+    const divTitulo= $('<div class="titulo"></div> ');
+    const titulo   = $('<h2>'+state.newTitle[0].title+'</h2>');
+    const subtitulo= $('<h4>'+state.newTitle[0].brief+'</h4>');
+
+    container.append(img);
+    divTitulo.append(titulo);
+    divTitulo.append(subtitulo);
+    container.append(divTitulo);
+
+    return container;
+}
+
+
